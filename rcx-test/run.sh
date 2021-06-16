7f39033f777cecafebf5400ca11ad716f61c9515
Ales Krenek <ljocha@ics.muni.cz>
2021-06-16 18:29:02 +0200

    elementary testing notebook

---
+++ rcx-test/run.sh
@@ -0,0 +1,10 @@
+docker run \
+-e HOME=/data \
+-e PYTHONPATH=/opt/callc/rt \
+-v $PWD:/data -w /data \
+-u $(id -u) \
+-p 8080:8080 \
+-ti \
+recetox/callc \
+bash -c '. /opt/conda/etc/profile.d/conda.sh && conda activate callc && jupyter notebook --port 8080 --ip 0.0.0.0'
+
